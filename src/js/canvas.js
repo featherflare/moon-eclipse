c11228abd3dda415600e5a21be7b23f76de69175
featherflare <max43607@gmail.com>
2025-02-21 08:31:36 +0700

    setOffset

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -27,6 +27,7 @@ cols = (canvas.width / moonRad) * 1.2
 rows = (canvas.height / moonRad) * 1.5
 let grid = []
 let w = canvas.width / cols
+let moonOffset = 5
 
 // Compute center
 const centerX = canvas.width / 2
@@ -42,21 +43,39 @@ for (let i = 0; i < cols; i++) {
       (canvas.width / cols) * i +
       canvas.width / cols / 2 +
       -moonRad / 1 +
-      (moonRad / 5) * i
+      (moonRad / moonOffset) * i
     let y =
-      (canvas.height / rows) * j + canvas.height / rows / 2 + (moonRad / 5) * j
+      (canvas.height / rows) * j +
+      canvas.height / rows / 2 +
+      (moonRad / moonOffset) * j
 
     let offset = j / cols
 
     let dist = Utils.distance(centerX, centerY, x, y) / maxDist
-    let angle = (Math.atan2(y - centerY, x - centerX) / Math.PI + 1) / 1
+    offset = dist
 
+    let angle = (Math.atan2(y - centerY, x - centerX) / Math.PI + 1) / 2
     offset = (dist * 1 + angle * 0.5) % 1
 
     if (j % 2 !== 0) {
       x += moonRad / 2
     }
-    grid.push({ x, y, offset })
+
+    let rad = moonRad / 2
+    // if (i < canvas.width / moonRad / 2) {
+    //   let minimum = moonRad * 0.9
+    //   let radOffset = (minimum * i) / (cols / 1.2 / 2)
+    //   console.log(minimum, radOffset)
+
+    //   rad = (moonRad - radOffset) / 2
+    // } else {
+    //   let minimum = moonRad * 0.9
+    //   let radOffset = (minimum * (cols / 1.2 - i)) / (cols / 1.2 / 2)
+    //   console.log(minimum, radOffset)
+    //   rad = (moonRad - radOffset) / 2
+    // }
+
+    grid.push({ x, y, rad, offset })
   }
 }
 
@@ -142,9 +161,10 @@ let objects
 
 function init() {
   let num = 100
-  let rad = moonRad / 2
 
-  objects = grid.map(({ x, y, offset }) => new Moon(x, y, num, rad, offset))
+  objects = grid.map(({ x, y, rad, offset }, index) => {
+    return new Moon(x, y, num, rad, offset)
+  })
 }
 
 // Animation Loop
